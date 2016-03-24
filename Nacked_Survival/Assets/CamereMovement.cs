4695eb1101156396af2226402c9b048f826b56a0
Ammon Sillah <Ammon Sillah>
2016-03-24 20:56:33 +0100

    Created Folder

---
+++ Nacked_Survival/Assets/CamereMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamereMovement : MonoBehaviour {
+
+	Transform target;
+	// Use this for initialization
+	void Start () {
+		target = GameObject.Find ("Player").transform;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = target.position + new Vector3(0,0,-30);
+	}
+}
